cfe266db207e4812fdff5af6cbaf74f2bae9201b
tmk_git-at-537713712211 <m39FKzXAEI0k/g1R/cE5kgdMV5zwh22/Nigw7SuvfeA=>
2023-10-17 11:04:43 +0200

    Datenbank verbunden, Listen versucht zu kombinieren

--- api/api.py
+++ api/api.py
@@ -1,6 +1,8 @@
 import flask
-from flask import request   # wird benötigt, um die HTTP-Parameter abzufragen
+from flask import request, json  # wird benötigt, um die HTTP-Parameter abzufragen
 from flask import jsonify   # übersetzt python-dicts in json
+import sqlite3
+#from itertools import izip
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True  # Zeigt Fehlerinformationen im Browser, statt nur einer generischen Error-Message
@@ -22,9 +24,33 @@ books = [
      'first_sentence': 'to wound the autumnal city.',
      'published': '1975'}
 ]
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
 @app.route('/', methods=['GET'])
+@app.errorhandler(404)
+def page_not_found():
+    return "<h1>404</h1><p>Es ist ein unbekannter Fehler aufgetreten.</p>", 404
+@app.route('/api/restaurant/table_reservations/all', methods=['GET'])
+def api_all():
+    conn = sqlite3.connect('buchungssystem.sqlite')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    all_tables = cur.execute('SELECT anzahlPlaetze FROM tische;').fetchall()
+    print(type(all_tables))
+    all_reservations = cur.execute('SELECT * FROM reservierungen;').fetchall()
+    reservationDict = dict.fromkeys(all_reservations)
+    print(type(all_reservations))
+    for reservation in reservationDict:
+        print(reservation)
+    return
+
+
 def home():
     return jsonify(books)
+
 
 app.run()
